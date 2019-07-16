c73aba50cadee63e9fe31f0d9beb36e1165d6926
Pablo Bautista <pablo@pixela.com.gt>
2019-07-16 12:51:24 -0600

    Example created

---
+++ example/main.dart
@@ -0,0 +1,23 @@
+import 'package:flusmic/flusmic.dart';
+
+main(List<String> args) async {
+  final flusmic = Flusmic(prismicEndpoint: 'yourendpoint');
+
+  ///Get API
+  final api = await flusmic.getApi();
+  print(api.toString());
+
+  ///Get root document
+  final root = await flusmic.getRootDocument();
+  print(root.toString());
+
+  ///Get document by type
+  final documentsResult = await flusmic.getDocumentById('type');
+  documentsResult.results.forEach((document) {
+    print(document.data.toString());
+  });
+
+  ///Get document by id
+  final documentResult = await flusmic.getDocumentById('id');
+  print(documentResult.results.first.data.toString());
+}
