829af9340de5e6d6a5edd8f3955d0bb1a2394ff7
Pablo Bautista <pablo@pixela.com.gt>
2020-04-07 22:05:10 -0600

    Orderings ready, test pending

---
+++ lib/src/models/ordering/ordering.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'ordering.freezed.dart';
+
+@freezed
+abstract class Ordering with _$Ordering {
+  const factory Ordering(String customType, String field,
+      {@Default(false) bool descending}) = _Ordering;
+
+  @override
+  String toString() =>
+      'my.${this.customType}.${this.field}' + (this.descending ? ' des' : '');
+}
